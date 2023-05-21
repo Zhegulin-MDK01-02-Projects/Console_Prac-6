88779a267a5164f60d6940cb21023f2b20b47179
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-05-21 22:39:31 +0300

    Add files via upload

---
+++ Консоль Практическое задание №6/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 10; // количество чисел для записи
+Random rnd = new Random();
+
+using (StreamWriter sw = new StreamWriter("accounts.txt"))
+{
+    for (int i = 0; i < n; i++)
+    {
+        int num = rnd.Next(100000); // генерируем случайное число от 0 до 99999
+        string numStr = num.ToString().PadLeft(5, '0'); // дополняем число нулями до 5 знаков
+        sw.WriteLine(numStr); // записываем число в файл
+    }
+}
+
+Console.WriteLine("Файл accounts.txt успешно создан и заполнен.");
+Console.ReadLine();
